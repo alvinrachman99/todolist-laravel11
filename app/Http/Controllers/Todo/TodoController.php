1119e7c15bc03273f532215bc9646c9ade6cef4b
Alvin Rachman Septian <alvinrachman16@gmail.com>
2024-11-12 15:46:21 +0700

    Insert Data

---
+++ app/Http/Controllers/Todo/TodoController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Todo;
+
+use App\Http\Controllers\Controller;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('todo.app');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'task' => 'required|min:3|max:25'
+            ],
+            [
+                'task.required' => 'Input task wajib di isikan',
+                'task.min' => 'Minimal input untuk task adalah 3 karakter',
+                'task.max' => 'Minimal input untuk task adalah 25 karakter',
+            ]
+        );
+
+        $data = [
+            'task' => $request->input('task'),
+        ];
+
+        Todo::create($data);
+        return redirect('/todo')->with('success', 'Berhasil simpan data.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\Todo\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/app', function () {
-    return view('todo/app');
-});
+Route::get('/todo', [TodoController::class, 'index']);
+Route::post('/todo', [TodoController::class, 'store']);
 
